561c2d50aa3c3ce9405a71644d1921112efe3856
Satrapade user <tevgeniou@ip-10-9-166-224.ec2.internal>
2014-01-17 10:54:46 +0000

    ui

--- tools/ui.R
+++ tools/ui.R
@@ -12,8 +12,9 @@ shinyUI(pageWithSidebar(
   
   sidebarPanel(
     
-    HTML("Please reload the web page any time the app crashes. <strong> When it crashes the screen turns into grey.</strong> If it only stops reacting it may be because of 
-heavy computation or traffic on the server, in which case you should simply wait. This is a test version. </h4>"),
+    HTML("Please reload the web page any time the app crashes. <strong> When it crashes the whole screen turns into grey.</strong> If it only stops reacting it may be because of 
+heavy computation or traffic on the server, in which case you should simply wait. Plots may at times fade: you do <strong>not</strong> 
+         need to reload the app when this happens, simply continue using the app.This is a test version. </h4>"),
     
     ###########################################################    
     # STEP 2.1: read the data
